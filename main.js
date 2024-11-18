61ca2337641ff2c990aeb489f2b7f726a8617346
manutosti <manutosti@gmail.com>
2024-11-18 21:22:38 +0100

    19/11/2024 web funcional

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-localStorage.clear();
 let i;
+
+(function () {
+
+  const tablacita = document.getElementById("cuerpotabla");
+  let retrievedScores = JSON.parse(localStorage.getItem("save"))
+  if (retrievedScores.length > 0) {
+
+    for (i = 0; i < retrievedScores.length; i++) {
+      tablacita.innerHTML += "<tr id='" + (i + 1) + "'><td>" + retrievedScores[i].nombre +
+        "</td><td>" + retrievedScores[i].apellidos +
+        "</td><td>" + retrievedScores[i].id +
+        "</td><td>" + retrievedScores[i].fechanacimiento +
+        "</td><td>" + retrievedScores[i].telefono +
+        "</td><td>" + retrievedScores[i].observaciones +
+        "</td><td>" + retrievedScores[i].fechacita +
+        "</td><td>" + retrievedScores[i].horacita + ":" + retrievedScores[i].minutocita +
+        "</td><td><button id='delete" + (i + 1) + "' onclick='borrarFila(event)'>Borrar</button>" +
+        "</td><td><button id='edit" + (i + 1) + "' onclick='editarFila(event)'>Editar</button></td></tr>";
+    }
+  } else {
+    tablacita.innerHTML += "<tr><td colspan='8'> dato vacio </td></tr>"
+  }
+})()
 
 /*Realizamos el formulario y lo pasamos a las tabla*/
 const form = document.getElementById("citaForm"); // get the form element
 //Cuando hacemos el formulario y le damos a Aceptar
 form.addEventListener("submit", e => {
-  // detecta que hemos pusado aceptar
+  // detecta que hemos pulsado aceptar
   e.preventDefault(); // nos mantiene en la pagina
   const {
     elements
   } = form; //metemos todos los datos en la constante elements
   let tabla = [];
   //if localstore true miramos su length para el codigo, else ponemos 1
-  if (JSON.parse(localStorage.getItem("save"))) {
+  if (JSON.parse(localStorage.getItem("save")).length > 0) {
     const output = {
       codigo: (JSON.parse(localStorage.getItem("save")).length + 1),
       nombre: elements.nombre.value,
@@ -27,25 +49,24 @@ form.addEventListener("submit", e => {
     };
     tabla = tabla.concat(JSON.parse(localStorage.getItem('save') || '[]'));
     tabla.push(output);
-
     localStorage.setItem("save", JSON.stringify(tabla)); // guardamos en localStore
     const tablacita = document.getElementById("cuerpotabla");
     let retrievedScores = JSON.parse(localStorage.getItem("save"))
-    tablacita.innerHTML += "<tr id=" + output.codigo + "><td>" + retrievedScores[output.codigo - 1].nombre +
+    tablacita.innerHTML += "<tr id='" + output.codigo + "'><td>" + retrievedScores[output.codigo - 1].nombre +
       "</td><td>" + retrievedScores[output.codigo - 1].apellidos +
       "</td><td>" + retrievedScores[output.codigo - 1].id +
       "</td><td>" + retrievedScores[output.codigo - 1].fechanacimiento +
       "</td><td>" + retrievedScores[output.codigo - 1].telefono +
-      "</td><td>" + retrievedScores[output.codigo - 1].observaciones + "</td><td>" +
+      "</td><td>" + retrievedScores[output.codigo - 1].observaciones +
       "</td><td>" + retrievedScores[output.codigo - 1].fechacita +
       "</td><td>" + retrievedScores[output.codigo - 1].horacita + ":" + retrievedScores[output.codigo - 1].minutocita +
-      "</td><td><button id='delete" + output.codigo + "'>Borrar</button>" +
-      "</td><td><button id='edit" + output.codigo + "'>Editar</button></td></tr>";
-    document.getElementById("delete" + output.codigo).addEventListener("click", borrarFila);
+      "</td><td><button id='delete" + output.codigo + "' onclick='borrarFila(event)'>Borrar</button>" +
+      "</td><td><button id='edit" + output.codigo + "' onclick='editarFila(event)'>Editar</button></td></tr>";
+
 
   } else {
     const output = {
-      codigo: 1,
+      codigo: (JSON.parse(localStorage.getItem("save")).length + 1),
       nombre: elements.nombre.value,
       apellidos: elements.apellidos.value,
       id: elements.id.value,
@@ -62,18 +83,16 @@ form.addEventListener("submit", e => {
     localStorage.setItem("save", JSON.stringify(tabla));
     const tablacita = document.getElementById("cuerpotabla");
     let retrievedScores = JSON.parse(localStorage.getItem("save"))
-    tablacita.innerHTML += "<tr id=" + output.codigo + "><td>" + retrievedScores[output.codigo - 1].nombre +
+    tablacita.innerHTML = "<tr id='" + output.codigo + "'><td>" + retrievedScores[output.codigo - 1].nombre +
       "</td><td>" + retrievedScores[output.codigo - 1].apellidos +
       "</td><td>" + retrievedScores[output.codigo - 1].id +
       "</td><td>" + retrievedScores[output.codigo - 1].fechanacimiento +
       "</td><td>" + retrievedScores[output.codigo - 1].telefono +
-      "</td><td>" + retrievedScores[output.codigo - 1].observaciones + "</td><td>" +
+      "</td><td>" + retrievedScores[output.codigo - 1].observaciones +
       "</td><td>" + retrievedScores[output.codigo - 1].fechacita +
       "</td><td>" + retrievedScores[output.codigo - 1].horacita + ":" + retrievedScores[output.codigo - 1].minutocita +
-      "</td><td><button id='delete" + output.codigo + "'>Borrar</button>" +
-      "</td><td><button id='edit" + output.codigo + "'>Editar</button></td></tr>";
-    document.getElementById("delete" + output.codigo).addEventListener("click", borrarFila);
-
+      "</td><td><button id='delete" + output.codigo + "' onclick='borrarFila(event)'>Borrar</button>" +
+      "</td><td><button id='edit" + output.codigo + "' onclick='editarFila(event)'>Editar</button></td></tr>";
   }
 });
 
@@ -81,14 +100,13 @@ form.addEventListener("submit", e => {
 function buscarcita() {
   const formulariocita = document.getElementById('buscarcita');
   const { elements } = formulariocita;
-  output = {
+  let output = {
     id: elements.cita.value
   };
 
   let retrievedScores = JSON.parse(localStorage.getItem("save"))
   i = 0;
   while (retrievedScores[i].id !== output.id && i < retrievedScores.length) {
-    console.log(retrievedScores[i].id.value);
     i++
   }
   let formulario = document.getElementById('actualizarForm');
@@ -131,32 +149,58 @@ function actualizarTabla() {
     localStorage.setItem("save", JSON.stringify(tabla)); // guardamos en localStore
     const tablacita = document.getElementById("cuerpotabla");
     let retrievedScores = JSON.parse(localStorage.getItem("save"));
-
-    document.getElementById(output.codigo).innerHTML = `<tr id=' fila${output.codigo}'><td>
-    ${retrievedScores[output.codigo - 1].nombre}</td><td> ${retrievedScores[output.codigo - 1].apellidos}
-    </td><td> ${retrievedScores[output.codigo - 1].id}
-    </td><td> ${retrievedScores[output.codigo - 1].fechanacimiento}
-    </td><td> ${retrievedScores[output.codigo - 1].telefono}
-    </td><td> ${retrievedScores[output.codigo - 1].observaciones}</td><td>
-    </td><td> ${retrievedScores[output.codigo - 1].fechacita}
-    </td><td> ${retrievedScores[output.codigo - 1].horacita} : ${retrievedScores[output.codigo - 1].minutocita}
-    </td><td><button id='delete${output.codigo}' >Borrar</button>
-    </td><td><button id='edit${output.codigo}' onclick=''>Editar</button></td></tr>`;
-    document.getElementById("delete" + output.codigo).addEventListener("click", borrarFila);
+    console.log(output.codigo);
+    document.getElementById(output.codigo).innerHTML = "<tr id='"+ output.codigo +"'><td>" + retrievedScores[output.codigo - 1].nombre +
+      "</td><td>" + retrievedScores[output.codigo - 1].apellidos +
+      "</td><td>" + retrievedScores[output.codigo - 1].id +
+      "</td><td>" + retrievedScores[output.codigo - 1].fechanacimiento +
+      "</td><td>" + retrievedScores[output.codigo - 1].telefono +
+      "</td><td>" + retrievedScores[output.codigo - 1].observaciones +
+      "</td><td>" + retrievedScores[output.codigo - 1].fechacita +
+      "</td><td>" + retrievedScores[output.codigo - 1].horacita + ":" + retrievedScores[output.codigo - 1].minutocita +
+      "</td><td><button id='delete" + output.codigo + "' onclick='borrarFila(event)'>Borrar</button>" +
+      "</td><td><button id='edit" + output.codigo + "' onclick='editarFila(event)'>Editar</button></td></tr>";
   }
 }
 
 //Boton para borrar fila
-function borrarFila(e) {
+function borrarFila(event) {
   let retrievedScores = JSON.parse(localStorage.getItem("save")); //cargamos el localStore
-  const index = retrievedScores.indexOf(e.target.parentElement.parentElement.id - 1);
+  let index;
   retrievedScores.splice(index, 1); //borramos el elemento en el array de localStore
   localStorage.setItem("save", JSON.stringify(retrievedScores)); //guardamos
-
-  const parent = e.target.parentElement.parentElement; //apuntamos a la fila
+  
+  const parent = event.target.parentElement.parentElement; //apuntamos a la fila
   parent.remove();//borramos la fila de la tabla
+  if (retrievedScores.length < 1) {
+    console.log("miau");
+    document.getElementById("cuerpotabla").innerHTML += "<tr><td colspan='8'> dato vacio </td></tr>"
+  }
 }
 
+//Boton para editar fila
+function editarFila(event){
+  console.log("hola1");
+  let retrievedScores = JSON.parse(localStorage.getItem("save")); //cargamos el localStore
+  let id=(event.target.parentElement.parentElement.id-1);
+  console.log(id);
+  const formulariocita = document.getElementById('buscarcita');
+  const { elements } = formulariocita;
+  console.log("hola2");
+  let formulario = document.getElementById('actualizarForm');
+  formulario.elements['nombre'].value = retrievedScores[id].nombre;
+  formulario.elements['apellidos'].value = retrievedScores[id].apellidos;
+  formulario.elements['id'].value = retrievedScores[id].id;
+  formulario.elements['fechanacimiento'].value = retrievedScores[id].fechanacimiento;
+  formulario.elements['telefono'].value = retrievedScores[id].telefono;
+  formulario.elements['observaciones'].value = retrievedScores[id].observaciones;
+  formulario.elements['fechacita'].value = retrievedScores[id].fechacita;
+  formulario.elements['hora'].value = retrievedScores[id].horacita;
+  formulario.elements['minuto'].value = retrievedScores[id].minutocita;
+  console.log("hola3");
+  return i = id;
+
+}
 
 
 /*Para fecha y hora de la cita no te deje poner una en el pasado*/
